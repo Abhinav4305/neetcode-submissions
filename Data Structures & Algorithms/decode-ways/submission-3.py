8e4cb2e56eb24407b0d790cf2265015f402ba748
Abhinav Bhargava <144687924+Abhinav4305@users.noreply.github.com>
2026-08-08 10:34:23 +0530

    Add: decode-ways - submission-3

---
+++ Data Structures & Algorithms/decode-ways/submission-3.py
@@ -0,0 +1,9 @@
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        for i in range(10):
+            if len(s) == 1 and int(s) == i:
+                return 1
+        if int(s) > 10:
+            return len(s)
+        else:
+            return 0
